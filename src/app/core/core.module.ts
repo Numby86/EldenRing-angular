52418e443f7d631efcb3de43e81ecf9bcfcd5a8a
Numby86 <numby86@gmail.com>
2023-02-06 15:12:58 +0100

    (feat)first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
